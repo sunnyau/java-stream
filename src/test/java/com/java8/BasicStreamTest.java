5d7fb402a1f9f4e99a89c8a88d058d9bf5979eb6
sunnyau <sunny.au@outlook.com>
2024-06-28 07:55:39 +0100

    add MinMaxSumAverageTest.java

--- src/test/java/com/java8/BasicStreamTest.java
+++ src/test/java/com/java8/BasicStreamTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.Optional;
 
 import org.junit.Test;
 
@@ -19,24 +18,21 @@ import org.junit.Test;
  * 
  * @author aus
  */
-public class BasicStreamTest
-{
+public class BasicStreamTest {
     private List<String> list = List.of("A1", "A2", "B1", "B2", "C1", "C2");
-    private List<Integer> numList = List.of(2, 3, 4, 5, 6);
+
     private Map<String, Integer> map = Map.of("A", 11, "B", 12, "C", 13, "D", 14, "E", 15);
 
     /**
      * Introduce stream(), collect(), toList(), toMap()
      */
     @Test
-    public void streamThenCollectShouldChangeNothing()
-    {
+    public void streamThenCollectShouldChangeNothing() {
         List<String> newList = list.stream().collect(Collectors.toList());
         assertEquals(list, newList);
 
-        Map<String, Integer> newMap =
-                map.entrySet().stream().collect(Collectors.toMap(p -> p.getKey(),
-                                                                 p -> p.getValue()));
+        Map<String, Integer> newMap = map.entrySet().stream()
+                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
 
         assertEquals(map, newMap);
     }
@@ -45,41 +41,35 @@ public class BasicStreamTest
      * filter has Predicate parameter (i.e. return boolean )
      */
     @Test
-    public void filterKey()
-    {
-        List<String> newList =
-                list.stream().filter(p -> p.startsWith("C")).collect(Collectors.toList());
+    public void filterKey() {
+        List<String> newList = list.stream().filter(p -> p.startsWith("C")).collect(Collectors.toList());
         assertEquals(2, newList.size());
 
-        Map<String, Integer> newMap =
-                map.entrySet().stream().filter(p -> p.getKey().equals("B")).collect(Collectors.toMap(p -> p.getKey(),
-                                                                                                     p -> p.getValue()));
+        Map<String, Integer> newMap = map.entrySet().stream().filter(p -> p.getKey().equals("B"))
+                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
         assertEquals(1, newMap.size());
     }
 
     /**
-     * map can transform structure. (e.g. List<String> to List<Integer>, Map to
-     * Set etc )
+     * map can transform structure. (e.g. List<String> to List<Integer>, Map to Set
+     * etc )
      */
     @Test
-    public void mapShouldTransform()
-    {
+    public void mapShouldTransform() {
         // transform List<String> to List<Integer>
-        List<Integer> countList =
-                list.stream().map(p -> p.length()).collect(Collectors.toList());
+        List<Integer> countList = list.stream().map(p -> p.length()).collect(Collectors.toList());
         List<Integer> expected1 = Arrays.asList(2, 2, 2, 2, 2, 2);
         assertEquals(expected1, countList);
 
         // transform Map<String,Integer> to Set<String> of key
-        Set<String> mapKeySet =
-                map.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toSet());
+        Set<String> mapKeySet = map.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toSet());
         // List<String> expected2 = Arrays.asList("A", "B", "C", "D", "E");
         Set<String> expected2 = Set.of("A", "B", "C", "D", "E");
         assertEquals(expected2, mapKeySet);
 
         // transform Map<String,Integer> to Set<String> of ( key + value )
-        Set<String> keyPlusValueSet =
-                map.entrySet().stream().map(p -> p.getKey() + p.getValue()).collect(Collectors.toSet());
+        Set<String> keyPlusValueSet = map.entrySet().stream().map(p -> p.getKey() + p.getValue())
+                .collect(Collectors.toSet());
         Set<String> expected3 = Set.of("A11", "B12", "C13", "D14", "E15");
         assertEquals(expected3, keyPlusValueSet);
     }
@@ -88,14 +78,13 @@ public class BasicStreamTest
      * transform list to map.
      */
     @Test
-    public void groupingBy()
-    {
+    public void groupingBy() {
         // Note : s.charAt(0) is a char, which is not an object. As you want key
         // to be String, we wrap it into String.valueOf() method. ( Also I
         // didn't check the length of the s to simplify the groupingBy
         // function.)
-        Map<String, List<String>> groupingByFirstCharMap =
-                list.stream().collect(Collectors.groupingBy(s -> String.valueOf(s.charAt(0))));
+        Map<String, List<String>> groupingByFirstCharMap = list.stream()
+                .collect(Collectors.groupingBy(s -> String.valueOf(s.charAt(0))));
 
         Map<String, List<String>> expected = new HashMap<>();
         expected.put("A", Arrays.asList("A1", "A2"));
@@ -109,50 +98,27 @@ public class BasicStreamTest
      * min and max use comparator and return Optional
      */
     @Test
-    public void countSumMinMaxWithOptionalEtc()
-    {
+    public void countSumMinMaxWithOptionalEtc() {
         // count
         assertEquals(5, map.values().stream().count());
 
         // sum
         assertEquals(65, map.values().stream().mapToLong(i -> i).sum());
-        assertEquals(10,
-                     map.values().stream().mapToLong(i -> String.valueOf(i).length()).sum());
+        assertEquals(10, map.values().stream().mapToLong(i -> String.valueOf(i).length()).sum());
 
         // min and max use comparator and return Optional
         final Comparator<Integer> compareByValue = Comparator.comparing(i -> i);
-        assertEquals(Integer.valueOf(11),
-                     map.values().stream().min(compareByValue).get());
+        assertEquals(Integer.valueOf(11), map.values().stream().min(compareByValue).get());
 
-        assertEquals(Integer.valueOf(15),
-                     map.values().stream().max(compareByValue).get());
+        assertEquals(Integer.valueOf(15), map.values().stream().max(compareByValue).get());
     }
 
     /**
      * accumulated = accumulated + " " + eachItem
      */
     @Test
-    public void reduceTakeTwoParameter()
-    {
-        String reduce1 =
-                list.stream().reduce((accumulated, i) -> accumulated + " " + i).get();
+    public void reduceTakeTwoParameter() {
+        String reduce1 = list.stream().reduce((accumulated, i) -> accumulated + " " + i).get();
         assertEquals("A1 A2 B1 B2 C1 C2", reduce1);
     }
-
-    @Test
-    public void reduceMinMaxSumOptional()
-    {
-        assertEquals( Optional.of(2), numList.stream().reduce(Integer::min) );                
-        assertEquals( Optional.of(6), numList.stream().reduce(Integer::max) );
-        assertEquals( Optional.of(20), numList.stream().reduce(Integer::sum) );
-    }
-
-    @Test
-    public void reduceMinMaxSum()
-    {
-        // interesting. it is 0, not 2
-        assertEquals( 0, (int)numList.stream().reduce(0, Integer::min) );
-        assertEquals( 6, (int)numList.stream().reduce(0, Integer::max) );
-        assertEquals( 20, (int)numList.stream().reduce(0, Integer::sum) );                
-    }    
 }
---
+++ src/test/java/com/java8/MinMaxSumAverageTest.java
@@ -0,0 +1,47 @@
+package com.java8;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class MinMaxSumAverageTest {
+
+    private List<Integer> numList = List.of(2, 3, 4, 5, 6);
+
+    @Test
+    public void reduceMinMaxSumOptional() {
+        assertEquals(Optional.of(2), numList.stream().reduce(Integer::min));
+        assertEquals(Optional.of(6), numList.stream().reduce(Integer::max));
+        assertEquals(Optional.of(20), numList.stream().reduce(Integer::sum));
+    }
+
+    @Test
+    public void reduceMinMaxSum() {
+        // interesting. it is 0, not 2
+        assertEquals(0, (int) numList.stream().reduce(0, Integer::min));
+        assertEquals(6, (int) numList.stream().reduce(0, Integer::max));
+        assertEquals(20, (int) numList.stream().reduce(0, Integer::sum));
+    }
+
+    /**
+     * By default, min and max return Optional Int
+     */
+    @Test
+    public void minMaxSumAverage() {
+        assertEquals(2,  numList.stream().mapToInt(Integer::valueOf).min().orElse(0));
+        assertEquals(6,  numList.stream().mapToInt(Integer::valueOf).max().orElse(0));
+        assertEquals(20, numList.stream().mapToInt(Integer::valueOf).sum());
+        assertEquals(4.0,  numList.stream().mapToInt(Integer::valueOf).average().orElse(0.0), 0.01);
+    }
+
+    @Test
+    public void IntStreamTest() {
+        assertEquals( 10, IntStream.rangeClosed(1, 10).count());
+        assertEquals( 9, IntStream.range(1, 10).count());
+    }
+
+}

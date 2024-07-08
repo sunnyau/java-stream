81f9317698a5fb428884f4ca215af367aaef867b
sunnyau <sunny.au@outlook.com>
2024-07-09 00:08:58 +0100

    fix TestRunner.java

--- src/it/java/tests/TestRunner.java
+++ src/it/java/tests/TestRunner.java
@@ -1,8 +1,8 @@
 package tests;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestRunner {
 
